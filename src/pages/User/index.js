3416db90560bc2231827889b335c460158da3ef8
Gabriel <gabriel.abner@gmail.com>
2019-11-28 20:15:07 -0300

    desafios

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+// import { ActivityIndicator } from 'react-native';
 import PropTypes from 'prop-types';
 import api from '../../services/api';
 import {
@@ -17,16 +18,52 @@ import {
 
 export default function User({ navigation }) {
   const [stars, setStars] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [page, setPage] = useState(1);
+  const [fullFavorites, setFullFavorites] = useState(false);
 
   async function loadUserFavoriteRepos() {
-    const user = navigation.getParam('user');
+    if (!loading) {
+      const user = navigation.getParam('user');
+      setLoading(true);
+      const response = await api.get(
+        `/users/${user.login}/starred?page=${page}`
+      );
 
-    const response = await api.get(`/users/${user.login}/starred`);
-    setStars(response.data);
+      if (response.data.length > 0) {
+        if (page === 1) {
+          setStars(response.data);
+        } else {
+          setStars([...stars, ...response.data]);
+        }
+      } else {
+        setFullFavorites(true);
+      }
+
+      setLoading(false);
+    }
   }
+
+  async function loadMore() {
+    if (!loading) {
+      if (stars.length >= 30) {
+        if (!fullFavorites) {
+          setPage(page + 1);
+        }
+      }
+    }
+  }
+
+  async function puxouPraBaixo() {
+    if (!loading) {
+      setFullFavorites(false);
+      setPage(1);
+    }
+  }
+
   useEffect(() => {
     loadUserFavoriteRepos();
-  }, []);
+  }, [page]);
 
   const user = navigation.getParam('user');
   return (
@@ -36,7 +73,12 @@ export default function User({ navigation }) {
         <Name>{user.name}</Name>
         <Bio>{user.bio}</Bio>
       </Header>
+      {/* loading ? <ActivityIndicator color="#7159c1" size="large" /> : null */}
       <Stars
+        onEndReachedThreshold={0.2}
+        onEndReached={loadMore}
+        onRefresh={puxouPraBaixo}
+        refreshing={loading}
         data={stars}
         keyExtractor={star => String(star.id)}
         renderItem={({ item }) => (
